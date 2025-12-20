9251299db297a3eabf75e554be4e50ffb1e58f28
Ryan Burns <ryankburns@gmail.com>
2025-12-19 17:02:54 -0800

    Initial Commit

---
+++ metal-cpp-tutorial/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  metal-cpp-tutorial
+//
+//  Created by ryan burns on 12/19/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
